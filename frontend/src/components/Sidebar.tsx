72dfa3376924f69f69f41f11543798d381ced114
Bert154-beep <masabayan15@gmail.com>
2026-06-05 02:09:36 +0500

    feat: added-sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { AlignJustify } from 'lucide-react'
+import React, { type Dispatch, type SetStateAction } from 'react'
 
-const Sidebar = () => {
+interface SidebarProps{
+    openSidebar: boolean,
+    setSidebar: Dispatch<SetStateAction<boolean>>
+}
+
+const Sidebar = ({openSidebar, setSidebar}: SidebarProps) => {
   return (
-    <div className='h-[100vh] w-[50vh]'>Sidebar</div>
+    <div className={`h-screen transition-all duration-300 ${openSidebar ? "w-64" : "w-16"} bg-[#080A0E]`}>
+      <div className='p-5 flex items-center gap-5'>
+        <button onClick={()=> setSidebar(prev => !prev)} className='cursor-pointer'><AlignJustify color='white'/></button>
+        {openSidebar && <p className='text-white font-syne text-lg'>Main Menu</p>}
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/CodeEditor.tsx
+++ frontend/src/pages/CodeEditor.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Editor } from '@monaco-editor/react'
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
 
 const CodeEditor = () => {
+
+    const [openSidebar, setSidebar] = useState<boolean>(true)
+
     return (
         <div className='w-full h-full'>
-            <Navbar/>
-        <div className='flex '>
-
-            <Sidebar />
-            <Editor
-                height="70vh"
-                width="100vw"
-                theme='vs-dark'
-                defaultLanguage='javascript'
-                defaultValue='Hello'
-                />
-        </div>
+            <Navbar />
+            <div className='flex w-full'>
+                <Sidebar openSidebar={openSidebar} setSidebar={setSidebar} />
+                <div className='flex-1 min-w-0'>
+                    <Editor
+                        height="70vh"
+                        width="100%"
+                        theme='vs-dark'
+                        defaultLanguage='javascript'
+                        defaultValue='Hello'
+                    />
+                </div>
+            </div>
         </div>
     )
 }
